d4f09840554d4357a41d897caa699ce017342998
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-02 11:01:47 +0100

    update style

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ import Map from 'components/Map';
 
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
@@ -58,7 +58,57 @@ const IndexPage = () => {
       })
     }
 
-    const geoJsonLayers = new L.GeoJSON(geoJson);
+    function countryPointToLayer(feature = {}, latlng) {
+      const { properties = {} } = feature;
+      let updatedFormatted;
+      let casesString;
+
+      const {
+        country,
+        updated,
+        cases,
+        deaths,
+        recovered
+      } = properties
+
+      casesString = `${cases}`;
+
+      if (cases > 1000) {
+        casesString = `${casesString.slice(0, -3)}k+`
+      }
+
+      if (updated) {
+        updatedFormatted = new Date(updated).toLocaleString();
+      }
+
+      const html = `
+      <span class="icon-marker">
+        <span class="icon-marker-tooltip">
+          <h2>${country}</h2>
+          <ul>
+            <li><strong>Confirmed:</strong> ${cases}</li>
+            <li><strong>Deaths:</strong> ${deaths}</li>
+            <li><strong>Recovered:</strong> ${recovered}</li>
+            <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+          </ul>
+        </span>
+        ${casesString}
+      </span>
+    `;
+
+      return L.marker(latlng, {
+        icon: L.divIcon({
+          className: 'icon',
+          html
+        }),
+        riseOnHover: true
+      });
+    }
+
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: countryPointToLayer
+    });
 
     geoJsonLayers.addTo(map);
 
@@ -80,12 +130,12 @@ const IndexPage = () => {
       <Map {...mapSettings} />
 
       <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
+        {/* <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <pre>
           <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
         </pre>
-        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+        <p className="note">Note: Gatsby CLI required globally for the above command</p> */}
       </Container>
     </Layout>
   );
