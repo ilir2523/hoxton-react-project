7bb43c7ec126ed1d53f9914c1a6e5b347d852b1e
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-02 12:36:15 +0100

    Mapbox Style Done

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -1,7 +1,12 @@
 export const mapServices = [
   {
-    name: 'OpenStreetMap',
+    name: 'Map',
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  },
+  {
+    name: 'OpenStreetMap',
+    attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    url: 'https://api.mapbox.com/styles/v1/ilir23/ckz49rw9j002014pef2h6el43/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaWxpcjIzIiwiYSI6ImNrejQ4endlMTBjeG4yb3J4NjR5cHdpZnEifQ.pLs5HIqLDbJA8NYdEy4KzA'
   }
 ];
