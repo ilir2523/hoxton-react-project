812ded27cfb5c9a2d6ff9463a10de2638f687fe6
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-02-03 19:58:09 +0100

    message

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/page-2/">Page 2</Link>
+            <Link to="/global-stats/">Global Stats</Link>
           </li>
         </ul>
       </Container>
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,15 +4,57 @@ import Helmet from 'react-helmet';
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 
+import { useTracker } from 'hooks';
+import { commafy } from '../lib/util';
+
 const SecondPage = () => {
+
+  const { data: stats = [] } = useTracker({
+    api: 'all'
+  });
+
+  const dashboardStats = [
+    {
+      primary: {
+        label: 'Total Cases',
+        value: stats ? commafy(stats?.cases) : '-',
+      },
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats ? commafy(stats?.casesPerOneMillion) : '-',
+      },
+    }
+  ]
+
   return (
     <Layout pageName="two">
       <Helmet>
-        <title>Page Two</title>
+        <title>Global Stats</title>
       </Helmet>
       <Container type="content" className="text-center">
-        <h1>Page Two</h1>
-        <p>Welcome to page 2</p>
+        <h1>Global Stats</h1>
+        <div className="new-stats">
+          <ul>
+            {dashboardStats.map(({ primary = {}, secondary = {} }, i) => {
+              return (
+                <li key={`Stat-${i}`} className="tracker-stat">
+                  {primary.value && (
+                    <p className="tracker-stat-primary">
+                      <strong>{primary.label}:</strong>
+                      {primary.value}
+                    </p>
+                  )}
+                  {secondary.value && (
+                    <p className="tracker-stat-secondary">
+                      <strong>{secondary.label}:</strong>
+                      {secondary.value}
+                    </p>
+                  )}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </Container>
     </Layout>
   );
